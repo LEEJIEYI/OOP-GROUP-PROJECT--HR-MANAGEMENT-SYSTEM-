1fc19d02891f69b94d6b0ed805cbfe8ec2bae906
LEEJIEYI <leejieyi05@gmail.com>
2025-06-22 00:43:58 +0800

    Add files via upload

--- HR MANAGEMENT SYSTEM/PAYROLL_SYSTEM/PayRollSystem.cs
+++ HR MANAGEMENT SYSTEM/PAYROLL_SYSTEM/PayRollSystem.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using Payroll_System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 
 namespace Payroll_System
@@ -18,32 +21,11 @@ namespace Payroll_System
         public Form1()
         {
             InitializeComponent();
-            StyleForm();
-        }
 
-        private void StyleForm()
-        {
-            // Form Styling
-            this.BackColor = Color.WhiteSmoke;
-            this.Text = "Payroll System";
-            this.Font = new Font("Segoe UI", 10);
-
-            // Header Label
-            label7.Text = "Payroll System";
-            label7.Font = new Font("Segoe UI", 18, FontStyle.Bold);
-            label7.ForeColor = Color.White;
-            label7.BackColor = Color.SteelBlue;
-            label7.TextAlign = ContentAlignment.MiddleCenter;
-            label7.Dock = DockStyle.Top;
-            label7.Height = 60;
-
-            // Label Styling
-            Label[] labels = { label1, label2, label3, label4, label5, label6 };
-            foreach (Label lbl in labels)
-            {
-                lbl.ForeColor = Color.DarkSlateGray;
-                lbl.Font = new Font("Segoe UI", 10, FontStyle.Bold);
-            }
+            this.BackColor = Color.FromArgb(245, 245, 245);
+            this.Font = new Font("Segoe UI", 10F);
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
 
             label1.Text = "Name";
             label2.Text = "Position";
@@ -51,29 +33,34 @@ namespace Payroll_System
             label4.Text = "Bonus";
             label5.Text = "Deductions";
             label6.Text = "Pay Slip Output";
+            label6.ForeColor = Color.Teal;
+            label7.Text = "Payroll System";
+            label7.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
+            label7.ForeColor = Color.DarkCyan;
 
-            // Button Styling
+            label1.ForeColor = Color.DarkSlateBlue;
+            label2.ForeColor = Color.DarkSlateBlue;
+            label3.ForeColor = Color.DarkSlateBlue;
+            label4.ForeColor = Color.DarkSlateBlue;
+            label5.ForeColor = Color.DarkSlateBlue;
+
+            txtName.BackColor = Color.WhiteSmoke;
+            txtPosition.BackColor = Color.WhiteSmoke;
+            txtBasicSalary.BackColor = Color.WhiteSmoke;
+            txtBonus.BackColor = Color.WhiteSmoke;
+            txtDeductions.BackColor = Color.WhiteSmoke;
+
+            txtOutput.BackColor = Color.Azure;
+            txtOutput.Font = new Font("Consolas", 10F);
+
             button1.Text = "Generate Pay Slip";
-            button1.BackColor = Color.MediumSeaGreen;
+            button1.BackColor = Color.Teal;
             button1.ForeColor = Color.White;
             button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
             button1.FlatAppearance.BorderSize = 0;
-            button1.Font = new Font("Segoe UI", 10, FontStyle.Bold);
 
-            // TextBox Styling
-            TextBox[] inputs = { txtName, txtPosition, txtBasicSalary, txtBonus, txtDeductions };
-            foreach (TextBox txt in inputs)
-            {
-                txt.BackColor = Color.White;
-                txt.ForeColor = Color.Black;
-                txt.Font = new Font("Segoe UI", 10);
-            }
-
-            // Output Box Styling
-            txtOutput.BackColor = Color.White;
-            txtOutput.ForeColor = Color.DarkGreen;
-            txtOutput.Font = new Font("Consolas", 10);
-            txtOutput.ReadOnly = true;
+            button1.Click += button1_Click;
         }
 
         private void button1_Click(object sender, EventArgs e)
--- HR MANAGEMENT SYSTEM/PAYROLL_SYSTEM/PaySlipList.cs
+++ HR MANAGEMENT SYSTEM/PAYROLL_SYSTEM/PaySlipList.cs
@@ -1,4 +1,4 @@
-using Payroll_System;
+﻿using Payroll_System;
 using System;
 using System.Collections.Generic;
 using System.Linq;
