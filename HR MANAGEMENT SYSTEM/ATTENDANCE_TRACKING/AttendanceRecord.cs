c4a0ad896e6e605b264752b8fc4b710f004a6cb5
LEEJIEYI <leejieyi05@gmail.com>
2025-06-21 23:19:21 +0800

    Add files via upload

---
+++ HR MANAGEMENT SYSTEM/ATTENDANCE_TRACKING/AttendanceRecord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AttendanceApp
+{
+    public class AttendanceRecord
+    {
+        public int EmployeeId { get; set; }
+        public DateTime CheckInTime { get; set; }
+        public DateTime? CheckOutTime { get; set; }
+
+        public TimeSpan? TotalHoursWorked =>
+            CheckOutTime.HasValue ? CheckOutTime.Value - CheckInTime : null;
+    }
+}
